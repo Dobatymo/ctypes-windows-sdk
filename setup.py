e94e8549fd200f7ed52fc0da293c4a357dc4f881
Dobatymo <dobatymo@users.noreply.github.com>
2019-03-11 17:56:22 +0800

    renamed

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-	name = 'python-windows-sdk',
+	name = 'ctypes-windows-sdk',
 	version = '0.14',
 	description = 'Ctypes port of Windows SDK',
-	packages = ["win32sdk"],
+	packages = ["cwinsdk"],
 	python_requires = ">=2.7",
 	use_2to3 = False,
 )
